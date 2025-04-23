88561a053f14f8b0bcc461f8a4820dbfa0c05bb4
Isaac Ochieng <43453935+IsaacOchi@users.noreply.github.com>
2025-04-23 13:28:08 +0300

    Close side bar when clicking on the overlay.
    Works but overlay is not visible.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const menuButton = document.querySelectorAll('.menu-button');
+const screenOverlay = document.querySelector('.screen-overlay');
 
 // Toggle sidebar visibility
 menuButton.forEach((button) => {
     button.addEventListener('click', () => {
         document.body.classList.toggle('sidebar-hidden');
     });
-}
-);
+});
+
+// Close sidebar when clicking outside of it
+screenOverlay.addEventListener('click', () => {
+    document.body.classList.toggle('sidebar-hidden');
+});
