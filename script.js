4c4abf3965d7f43a554dc374c11e547fcbd850d8
Isaac Ochieng <43453935+IsaacOchi@users.noreply.github.com>
2025-04-23 13:53:40 +0300

    Enable dark mode button to function.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const menuButton = document.querySelectorAll('.menu-button');
 const screenOverlay = document.querySelector('.screen-overlay');
+const themeButton = document.querySelector('.theme-button i');
+
+// Toggle dark mode
+themeButton.addEventListener('click', () => {
+    const isDarkMode = document.body.classList.toggle('dark-mode');
+    themeButton.classList.toggle('uil-sun', isDarkMode);
+    themeButton.classList.toggle('uil-moon', !isDarkMode);
+});
 
 // Toggle sidebar visibility
 menuButton.forEach((button) => {
