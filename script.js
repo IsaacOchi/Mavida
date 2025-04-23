32c7c20bb23f68de8d96aa681620c4c3d272aab2
Isaac Ochieng <43453935+IsaacOchi@users.noreply.github.com>
2025-04-23 14:01:09 +0300

    Enable local storage for the app.

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ const menuButton = document.querySelectorAll('.menu-button');
 const screenOverlay = document.querySelector('.screen-overlay');
 const themeButton = document.querySelector('.theme-button i');
 
+// Check for dark mode preference in local storage
+if (localStorage.getItem('dark-mode') === 'enabled') {
+    document.body.classList.add('dark-mode');
+    themeButton.classList.replace('uil-moon', 'uil-sun');
+} else {
+    themeButton.classList.replace('uil-sun', 'uil-moon');
+}
+
 // Toggle dark mode
 themeButton.addEventListener('click', () => {
     const isDarkMode = document.body.classList.toggle('dark-mode');
+    localStorage.setItem('dark-mode', isDarkMode ? 'enabled' : 'disabled');
     themeButton.classList.toggle('uil-sun', isDarkMode);
     themeButton.classList.toggle('uil-moon', !isDarkMode);
 });
